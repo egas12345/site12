593f925e79244a161452f7650f27d72076d74cc6
egas12345 <Egas12345@mail.ru>
2025-01-23 00:08:58 +0300

    src

---
+++ src/components/layouts/showcaseLayouts/ShowcaseFooter/ShowcaseFooter.tsx
@@ -0,0 +1,7 @@
+import classes from './ShowcaseFooter.module.css';
+
+const ShowcaseFooter: React.FC = () => {
+  return <footer className={classes.footer}>© 2024 Интернет-программирова</footer>;
+};
+
+export default ShowcaseFooter;
